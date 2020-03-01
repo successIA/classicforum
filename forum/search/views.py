001ad08f17b8d8c4afb1eff54c8517732c4b6254
Success99 <success99.ia@gmail.com>
2020-03-01 01:41:09 +0100

    Add search filter and refactor some files

--- forum/search/views.py
+++ forum/search/views.py
@@ -2,28 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from forum.threads.models import Thread
+from forum.comments.models import Comment
 from forum.threads.utils import get_paginated_queryset
-from forum.core.constants import THREAD_PER_PAGE
+from forum.core.constants import THREAD_PER_PAGE as PER_PAGE
 from forum.core.utils import add_pagination_context
 
 
 def search(request, page=1):
     query = request.GET.get('q')
     query =  query.strip() if query else ''
+    is_comment = None
     results = None
     ctx = {}
-    thread_paginator = None
-    if query:        
-        thread_qs = Thread.objects.active().filter(
-            title__icontains=query
-        ).order_by('-final_comment_time').distinct()
-        thread_paginator = get_paginated_queryset(
-            thread_qs, THREAD_PER_PAGE, page)
+    paginator = None
+    if query:
+        queryset = None
+        if request.GET.get('search_filter') == 'comment':
+            queryset = Comment.objects.active().filter(
+                message__icontains=query
+            ).order_by('-modified').distinct()
+            is_comment = True
+        else:        
+            queryset = Thread.objects.active().filter(
+                title__icontains=query
+            ).order_by('-final_comment_time').distinct()
+        paginator = get_paginated_queryset(
+            queryset, PER_PAGE, page)
         base_url = ['/search/', f'/?q={query}']
         ctx['base_url'] = base_url
-        add_pagination_context(base_url, ctx, thread_paginator)
+        add_pagination_context(base_url, ctx, paginator)
     ctx.update({ 
         'query': query,
-        'results': thread_paginator 
+        'results': paginator,
+        'is_comment': is_comment
     })
     return render(request, 'search/search.html', ctx)    
